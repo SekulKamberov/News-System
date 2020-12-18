125fddd8c2f7fbd8f9495fb4cc0272e01c50b413
Sekul Kamberov <microfixbg@gmail.com>
2020-12-18 15:20:35 +0200

    Comment model

---
+++ NewsSystem/NewsSystem.Domain/ArticleCreation/Models/Articles/Comment.cs
@@ -0,0 +1,27 @@
+﻿namespace NewsSystem.Domain.ArticleCreation.Models.Articles
+{
+    using NewsSystem.Domain.Common.Models;
+
+    public class Comment : Entity<int>
+    {
+        internal Comment(
+           string title,
+           string content,
+           string createdBy,
+           int articleId) 
+        {
+            this.Title = title;
+            this.Content = content;
+            this.CreatedBy = createdBy;
+            this.ArticleId = articleId;
+        }
+
+        public string Title { get; private set; }
+
+        public string Content { get; private set; }
+
+        public string CreatedBy { get; private set; }
+
+        public int ArticleId { get; private set; }
+    }
+}
