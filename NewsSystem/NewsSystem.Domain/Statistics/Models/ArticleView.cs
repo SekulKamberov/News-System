2ed822b67268184d7e75471338cd313276eadc8f
Sekul Kamberov <microfixbg@gmail.com>
2020-12-22 14:34:43 +0200

    Statistics AggregateRoot created
    Statistics AggregateRoot created

---
+++ NewsSystem/NewsSystem.Domain/Statistics/Models/ArticleView.cs
@@ -0,0 +1,17 @@
+﻿namespace NewsSystem.Domain.Statistics.Models
+{
+    using NewsSystem.Domain.Common.Models;
+
+    public class ArticleView : Entity<int>
+    {
+        internal ArticleView(int articleId, string? userId)
+        {
+            this.ArticleId = articleId;
+            this.UserId = userId; 
+        }
+
+        public int ArticleId { get; }
+
+        public string? UserId { get; }
+    }
+}
---
+++ NewsSystem/NewsSystem.Domain/Statistics/Models/Statisrics.cs
@@ -0,0 +1,28 @@
+﻿namespace NewsSystem.Domain.Statistics.Models
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using NewsSystem.Domain.Common;
+
+    public class Statisrics : IAggregateRoot
+    {
+        private readonly HashSet<ArticleView> articleViews;
+
+        internal Statisrics()
+        {
+            this.TotalArticles = 0;
+            this.articleViews = new HashSet<ArticleView>();
+        }
+
+        public int TotalArticles { get; private set; }
+
+        public IReadOnlyCollection<ArticleView> ArticleViews
+            => this.articleViews.ToList().AsReadOnly();
+
+        public void AddArticles()
+            => this.TotalArticles++;
+
+        public void AddArticleViews(int articleId, string? userrId)
+            => this.articleViews.Add(new ArticleView(articleId, userrId));      
+    }
+}
